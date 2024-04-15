55b83315f77912b1798353739f4b55f4a899faeb
RobMeades <rob@meades.org>
2024-04-15 09:12:18 +0100

    Platform change for STM32 only: add support for STM32U5 on STM32Cube.
    The STM32U5 MCU, which is already supported via Zephyr, is now also supported on the STM32Cube platform.
    
    CURRENT LIMITATIONS:
    - I2C and SPI are not yet implemented,
    - there appears to be a bug in the ST-provided CMSIS layer for ThreadX in that it does not free memory when a dynamic task is terminated, hence you will likely run out of memory; this is being progressed in https://github.com/STMicroelectronics/STM32CubeU5/issues/48.
    
    EXISTING STM32Cube USERS PLEASE NOTE: the guts of stm32f4.mk is moved up a directory-level and renamed to stm32.mk; it is included in stm32f4.mk so you _should_ notice no difference.
    
    Two configurations are supported: the default STM32U5 integration from ST, which now uses ThreadX as the underlying OS, and a configuration which continues to use FreeRTOS as the underlying OS (i.e. as STM32F4 does).  In order to make this possible all OS operations for STM32U5 builds go through CMSIS (V2).  See the README.md files under the stm32cube platform directory for details of how the two differ and what limitations you may find.

--- port/platform/common/automation/tasks/__init__.py
+++ port/platform/common/automation/tasks/__init__.py
@@ -1,6 +1,6 @@
 from invoke import Collection, Config
 from scripts import u_utils
-from . import nrfconnect, nrf5, stm32cubef4, esp_idf, arduino, automation, linux
+from . import nrfconnect, nrf5, stm32cube, esp_idf, arduino, automation, linux
 
 ns = Collection()
 
@@ -12,7 +12,7 @@ cfg['cfg_dir'] = u_utils.AUTOMATION_DIR
 ns.configure(cfg)
 ns.add_collection(nrfconnect)
 ns.add_collection(nrf5)
-ns.add_collection(stm32cubef4)
+ns.add_collection(stm32cube)
 ns.add_collection(esp_idf)
 ns.add_collection(arduino)
 ns.add_collection(automation)
